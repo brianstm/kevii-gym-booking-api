560418e78b0a7324842bfd01ed874f0f63d37267
BriansTjipto <brianstjipto@gmail.com>
2024-10-14 10:59:41 +0800

    Refactor database connection handling in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,40 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./config/database";
+// import connectDB from "./config/database";
 import authRoutes from "./routes/authRoutes";
 import bookingRoutes from "./routes/bookingRoutes";
 import checkInRoutes from "./routes/checkInRoutes";
+import mongoose from "mongoose";
+
+mongoose.set("strictQuery", false);
+
+const connectDB = async (): Promise<void> => {
+  const uri = process.env.MONGODB_URI;
+
+  if (!uri) {
+    console.error("MongoDB URI is not defined");
+    process.exit(1);
+  }
+
+  try {
+    const conn = await mongoose.connect(uri, {
+      serverSelectionTimeoutMS: 5000,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error("MongoDB connection error:", error);
+      if ("reason" in error) {
+        console.error("Error reason:", (error as any).reason);
+      }
+    } else {
+      console.error(
+        "An unknown error occurred while connecting to the database"
+      );
+    }
+    process.exit(1);
+  }
+};
 
 dotenv.config();
 
