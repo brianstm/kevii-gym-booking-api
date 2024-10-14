27a36ee885ec58011e95315bd4c39774d198a849
BriansTjipto <brianstjipto@gmail.com>
2024-10-14 11:08:25 +0800

    Refactor database connection handling and update package dependencies for date and time handling

--- src/app.ts
+++ src/app.ts
@@ -1,40 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-// import connectDB from "./config/database";
+import connectDB from "./config/database";
 import authRoutes from "./routes/authRoutes";
 import bookingRoutes from "./routes/bookingRoutes";
 import checkInRoutes from "./routes/checkInRoutes";
-import mongoose from "mongoose";
-
-mongoose.set("strictQuery", false);
-
-const connectDB = async (): Promise<void> => {
-  const uri = process.env.MONGODB_URI;
-
-  if (!uri) {
-    console.error("MongoDB URI is not defined");
-    process.exit(1);
-  }
-
-  try {
-    const conn = await mongoose.connect(uri, {
-      serverSelectionTimeoutMS: 5000,
-    });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.error("MongoDB connection error:", error);
-      if ("reason" in error) {
-        console.error("Error reason:", (error as any).reason);
-      }
-    } else {
-      console.error(
-        "An unknown error occurred while connecting to the database"
-      );
-    }
-    process.exit(1);
-  }
-};
 
 dotenv.config();
 
@@ -48,6 +17,10 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/", bookingRoutes);
 app.use("/api/checkin", checkInRoutes);
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "KEVII GYM BOOKING API", version: "1.0.0" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
